314ade37f9b8b273a66f6679600a461fee4e8f73
grievous0 <jvopereira2004@gmail.com>
2023-10-30 01:55:34 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World')
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
