f601aeffa996505911cc285ab2231c50028a985e
João Vitor de Oliveira Pereira <jvopereira2004@gmail.com>
2023-11-02 02:28:59 -0300

    Update protocols.ts

--- src/controllers/create-users/protocols.ts
+++ src/controllers/create-users/protocols.ts
@@ -1,4 +1,9 @@
 import { User } from "../../models/user";
+import { HttpResponse } from "../protocols";
+
+export interface ICreateUserController {
+    handle(): Promise<HttpResponse<User>>;
+}
 
 export interface CreateUserParams {
     firstName: string;
